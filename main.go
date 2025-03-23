11bc935133b47f59a48003b834731d19f22b331e
robotism <fan@gankcode.com>
2025-03-23 22:37:36 +0800

    update

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import casdoor "github.com/chaos-plus/lib3rd-casdoor/casdoor"
 
 func main() {
-	casdoor.ExportWebStaticFile("")
+	casdoor.ExportWebStaticFile("./casdoor/static/web")
 	casdoor.RunServer()
 }
